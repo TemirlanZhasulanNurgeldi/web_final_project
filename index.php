d4c5c3feadbb50927adee68d4382109b61d9e603
Temiirlan <43068971+Temiirlan@users.noreply.github.com>
2018-12-23 03:10:30 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -7,25 +7,6 @@
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all">
 <link href="css/display.css" rel="stylesheet" type="text/css" >
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Youth Fashion Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<link href='//fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-<link href='//fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
-   <script>
-    $(function () {
-      $("#slider").responsiveSlides({
-      	auto: false,
-      	speed: 500,
-        namespace: "callbacks",
-        pager: true,
-      });
-    });
-  </script>
-  <script>
- new WOW().init();
-</script>
 </head>
 <body>
 <div class="header">
@@ -50,18 +31,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			<div class="n-avigation">
 				<nav class="navbar nav_bottom" role="navigation">
 					<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
-						<ul class="nav navbar-nav nav_1">
-							<li><a href="index.php">Главная</a></li>
-							<li class="dropdown mega-dropdown active">
-								<a href="women.php" class="dropdown-toggle" data-toggle="dropdown">Женская<span class="caret"></span></a>				
-							</li>
-							<li class="dropdown mega-dropdown active">
-								<a href="men.php" class="dropdown-toggle" data-toggle="dropdown">Мужская<span class="caret"></span></a>						
-							</li>
-							<li><a href="products.php">Товары</a></li>
-							<li class="last"><a href="https://docs.google.com/forms/d/e/1FAIpQLSdpT5LjtEBk5XJf_ARz9dGKimPEy5WAg1x2nftYHBX6NxmnFQ/viewform?embedded=true" target="_blank" width="700" height="520" frameborder="0" marginheight="0" marginwidth="0"><p><button class="w3-button w3-black" type="submit"> Обратная связь</span></a>	
-							</li>
-							
+					<ul class="nav navbar-nav nav_1">
+              				<li><a href="index.html">Главная</a></li>
+              				<li><a href="women.php">Женская</a></li>
+            			  	<li><a href="men.php">Мужская</a></li>
+              				<li><a href="products.php">Товары</a></li>
+              				<li class="last"><a href="https://docs.google.com/forms/d/e/1FAIpQLSdpT5LjtEBk5XJf_ARz9dGKimPEy5WAg1x2nftYHBX6NxmnFQ/viewform?embedded=true" target="_blank" width="700" height="520" frameborder="0" marginheight="0" marginwidth="0"><p><button class="w3-button w3-black" type="submit">Обратная связь</span></a></li>					
 							<li class='active' style='float:right;'>
   <?php 
   if($_SESSION['user']==true)
@@ -79,37 +54,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				  
 				</nav>
 			</div>
-		<div class="clearfix"> </div>
-					<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all"/>
-					<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
-				<div id="small-dialog" class="mfp-hide">
-				<div class="search-top">
-						<div class="login">
-							<form action="#" method="post">
-								<input type="submit" value="">
-								<input type="text" name="search" value="Type something..." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '';}">		
-							
-							</form>
-						</div>
-						<p>	Shopping</p>
-					</div>				
-				</div>
-				 <script>
-						$(document).ready(function() {
-						$('.popup-with-zoom-anim').magnificPopup({
-							type: 'inline',
-							fixedContentPos: false,
-							fixedBgPos: true,
-							overflowY: 'auto',
-							closeBtnInside: true,
-							preloader: false,
-							midClick: true,
-							removalDelay: 300,
-							mainClass: 'my-mfp-zoom-in'
-						});
-																						
-						});
-				</script>			
 		</div>
 	</div>
 </div>
@@ -120,7 +64,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	      		<ul class="rslides" id="slider">
 	        		<li>
 	          			<img src="images/1.jpg" alt="">
-						<div class="tes animated wow fadeInUp animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+						<div class="tes animated wow fadeInUp animated">
 							<h2>MEN & WOMEN</h2>
 							<h3>Trousers & Chinos</h3>
 							<p>OFFER</p>
@@ -128,7 +72,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	       			 </li>
 			 		 <li>
 	          			<img src="images/3.jpg" alt=""> 
-						<div class="tes animated wow fadeInUp animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+						<div class="tes animated wow fadeInUp animated">
 							<h2>MEN & WOMEN</h2>
 							<h3>Trousers & Chinos</h3>
 							<p>OFFER</p>
@@ -136,7 +80,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	       			 </li>
 					 <li>
 	          			<img src="images/2.jpg" alt="">
-						<div class="tes animated wow fadeInUp animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+						<div class="tes animated wow fadeInUp animated">
 							<h2>MEN & WOMEN</h2>
 							<h3>Trousers & Chinos</h3>
 							<p>OFFER</p>
@@ -150,7 +94,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 </div>
 <div class="content">
 	<div class="container">
-		<div class="content-top">
 			<div class="content-top1">
 				<div class="col-md-3 col-md2 animated wow fadeInLeft">
 					<div class="col-md1 simpleCart_shelfItem">
@@ -248,7 +191,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	<div class="con-tp">
 		<div class="container">
 			<div class="content-top2">
-			<div class="col-md-4 con-tp-lft animated wow fadeInLeft" data-wow-delay=".5s">
+			<div class="col-md-4 con-tp-lft animated wow fadeInLeft">
 				<a href="products.php">
 					<div class="content-grid-effect slow-zoom vertical">
 						<div class="img-box"><img src="images/6.jpg" alt="image" class="img-responsive zoom-img"></div>
@@ -285,7 +228,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</a>
 			</div>
 			</div>
-			<div class="clearfix"></div>
 			<div class="content-top2">
 		<div class="col-md-4 con-tp-lft animated wow fadeInLeft" data-wow-delay=".5s">
 			<a href="products.php">
@@ -328,6 +270,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		</div>
 	</div>
 	<div class="c-btm">
+		<div class="c-btm">
 		<div class="content-top3">
 			<div class="container">
 				<div class="col-md-3 col-md2 animated wow fadeInLeft">
@@ -388,6 +331,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</div>	
 			</div>	
 		</div>
+	</div>
 	</div>
 <div class="footer">
 	<div class="container">
